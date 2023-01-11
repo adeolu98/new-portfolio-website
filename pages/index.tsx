c20f969c1cc33e1a1f4965318fb90f237051527f
dCRYPT <39372980+adeolu98@users.noreply.github.com>
2023-01-11 17:46:27 +0100

    improve responsiveness

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 
 export default function Home() {
   return (
-    <div className="bg-slate-100 px-32 py-20 w-screen h-screen">
+    <div className="bg-slate-100 px-8 py-10 lg:px-32 lg:py-20 w-screen h-screen">
       <div className="flex flex-row justify-between">
         <div className="flex flex-row gap-4">
           <FontAwesomeIcon
@@ -26,7 +26,7 @@ export default function Home() {
           <p className="">Send a mail</p>
         </div>
       </div>
-      <div className="flex justify-center items-center -mt-60">
+      <div className="flex justify-center items-center mt-52 lg:-mt-60">
         <motion.div
           transition={{
             duration: 8,
@@ -35,11 +35,11 @@ export default function Home() {
           }}
           initial={{ scale: 0.8 }}
           animate={{ scale: 1.3 }}
-          className="w-[900px] h-[900px] p-20 rounded-full border border-slate-300"
+          className="w-96 h-96 absolute lg:relative lg:w-[900px] lg:h-[900px] p-5 lg:p-20 rounded-full border border-slate-300"
         >
-          <div className="w-full h-full p-20 rounded-full border border-slate-300">
-            <div className="w-full h-full p-20 rounded-full border border-slate-300">
-              <div className="w-full h-full p-32 rounded-full border border-slate-300">
+          <div className="w-full h-full p-5 lg:p-20 rounded-full border border-slate-300">
+            <div className="w-full h-full p-5 lg:p-20 rounded-full border border-slate-300">
+              <div className="w-full h-full p-5 lg:p-32 rounded-full border border-slate-300">
                 <div className="w-full h-full rounded-full">
                   <img
                     alt="image"
@@ -52,22 +52,22 @@ export default function Home() {
           </div>
         </motion.div>
         
-      <div className="absolute flex flex-col items-center bottom-44">
+      <div className="absolute flex flex-col gap-8 items-center bottom-44">
         <div className="text-center">
           <p className="text-xl tracking-[.24em] ">SOFTWARE ENGINEER</p>
           <p className="mt-2 font-black text-4xl">ADEOLU ADEWOLE</p>
         </div>
-        <div className="mt-8 flex flex-row  gap-8 items-center justify-center">
-          <button className="px-6 rounded-3xl py-2 border-2 border-black">
+        <div className="flex flex-row gap-4 lg:gap-8 text-xs md:text-base items-center justify-center h-12 border">
+          <button className="px-2 lg:px-6 rounded-3xl py-2 hover:bg-slate-200 hover:border-2 hover:border-black focus:border-2 focus:border-black">
             <p>EXPERIENCE</p>
           </button>
-          <button>
+          <button className="px-2 lg:px-6 rounded-3xl py-2 hover:bg-slate-200  hover:border-2 hover:border-black focus:border-2 focus:border-black">
             <p> SKILLS</p>
           </button>
-          <button>
+          <button className="px-2 lg:px-6 rounded-3xl py-2 hover:bg-slate-200 hover:border-2 hover:border-black focus:border-2 focus:border-black">
             <p>PROJECTS</p>
           </button>
-          <button>
+          <button className="px-2 lg:px-6 rounded-3xl py-2 hover:bg-slate-200 hover:border-2 hover:border-black focus:border-2 focus:border-black">
             <p>ABOUT</p>
           </button>
         </div>
