68d9583b3e409518b3a7cbdcd791c6c6b9aac986
dCRYPT <39372980+adeolu98@users.noreply.github.com>
2023-01-22 12:03:39 +0100

    add sticky nav, add scroll behaviour

---
+++ components/Nav.tsx
@@ -0,0 +1,39 @@
+import React, { FunctionComponent } from "react";
+
+
+export interface NavProps {
+  className?: string;
+}
+
+export const Nav: FunctionComponent<NavProps> = ({
+  className = "",
+}) => {
+  return (
+    <div className={`${className} sticky top-1 flex flex-row gap-4 lg:gap-8 text-xs md:text-base items-center justify-center h-12`}>
+    <a
+      href="#experience"
+      className="px-2 lg:px-6 rounded-3xl py-2 hover:bg-slate-200 hover:border-2 hover:border-black focus:border-2 focus:border-black"
+    >
+      <p>EXPERIENCE</p>
+    </a>
+    <a
+      href="#skills"
+      className="px-2 lg:px-6 rounded-3xl py-2 hover:bg-slate-200  hover:border-2 hover:border-black focus:border-2 focus:border-black"
+    >
+      <p> SKILLS</p>
+    </a>
+    <a
+      href="#projects"
+      className="px-2 lg:px-6 rounded-3xl py-2 hover:bg-slate-200 hover:border-2 hover:border-black focus:border-2 focus:border-black"
+    >
+      <p>PROJECTS</p>
+    </a>
+    <a
+      href="#about"
+      className="px-2 lg:px-6 rounded-3xl py-2 hover:bg-slate-200 hover:border-2 hover:border-black focus:border-2 focus:border-black"
+    >
+      <p>ABOUT</p>
+    </a>
+  </div>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import { Skills } from "../components/Skills";
 import { Projects } from "../components/Projects";
 import { Experience } from "../components/Experience";
 import { About } from "../components/About";
+import { Nav } from "../components/Nav";
+import { Button } from "@mantine/core";
 
 export default function Home() {
   return (
@@ -12,22 +14,34 @@ export default function Home() {
         <ContactLinks />
         <Intro className="h-screen"></Intro>
       </div>
-      <div className="lg:h-screen w-full px-8 lg:px-32 flex flex-col items-center">
-        <p className="absolute text-4xl md:mt-6 text-center font-bold">
+      <Nav className="-mt-60 mb-20 h-12"></Nav>
+      <div
+        id="experience"
+        className="lg:h-screen w-full px-8 lg:px-32 flex flex-col items-center"
+      >
+        <p className="absolute text-4xl mt-20 lg:mt-24 text-center font-bold">
           Work Experience
         </p>
-        <Experience className="mt-20 w-full h-full"></Experience>
+        <Experience className="mt-20 lg:mt-28 w-full h-full"></Experience>
       </div>
-      <div className="w-full px-8 lg:px-32 flex flex-col justify-center py-16">
-        <p className="text-4xl text-center  font-bold">Skills</p>
+      <div
+        id="skills"
+        className="w-full px-8 lg:px-32 flex flex-col justify-center py-16"
+      >
+        <p className="mt-12 text-4xl text-center  font-bold">Skills</p>
         <Skills className="mt-20"></Skills>
       </div>
 
-      <div className="w-full gap-24 lg:gap-10 flex w-full flex-col py-20 justify-center px-8 lg:px-32 lg:py-32">
+      <div
+        id="projects"
+        className="w-full gap-24 lg:gap-10 flex w-full flex-col py-20 justify-center px-8 lg:px-32 lg:py-32"
+      >
         <p className="text-center text-4xl font-bold">Projects</p>
         <Projects className="w-full h-full"></Projects>
       </div>
-      <About></About>
+      <div id="about">
+        <About></About>
+      </div>
 
       <div className="py-3">
         <p className="text-center"> copyright &copy; 2022 ADEOLUADEWOLE</p>
