a60fcc07a452e42ddcdc1108cdbd2d7cd9242946
dCRYPT <39372980+adeolu98@users.noreply.github.com>
2023-01-25 11:16:04 +0100

    Update Skills.tsx

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -16,69 +16,69 @@ export interface SkillsProps {
 
 export const Skills: FunctionComponent<SkillsProps> = ({ className = "" }) => {
   return (
-    <div className={`${className} grid gap-4 grid-cols-3  md:grid-cols-6`}>
-      <SkillCard className="h-60">
+    <div className={`${className} grid gap-4 grid-cols-3  md:grid-cols-6 text-sm md:text-base lg:text-xl`}>
+      <SkillCard className="h-full lg:h-60">
         <img src={"/skills/tailwindcss.svg"} className="w-full h-full"></img>
-        <p className="py-4 text-xl">Tailwind</p>
+        <p className="py-4">Tailwind</p>
       </SkillCard>
-      <SkillCard className="h-60">
+      <SkillCard className="h-full lg:h-60">
         <img src={"/skills/typescript.svg"} className="w-full h-full"></img>
-        <p className="py-4 text-xl">Typescript</p>
+        <p className="py-4">Typescript</p>
       </SkillCard>
-      <SkillCard className="h-60">
+      <SkillCard className="h-full lg:h-60">
         <img src={"/skills/nextjs.svg"} className="w-full h-full"></img>
-        <p className="py-4 text-xl">Next.js</p>
+        <p className="py-4">Next.js</p>
       </SkillCard>
-      <SkillCard className="h-60">
+      <SkillCard className="h-full lg:h-60">
           <img src={"/skills/solidity.svg"} className="w-full h-full"></img>
-          <p className="py-4 text-xl">Solidity</p>
+          <p className="py-4">Solidity</p>
       </SkillCard>
-      <SkillCard className="h-60">
+      <SkillCard className="h-full lg:h-60">
         <img src={"/skills/hardhat.svg"} className="w-full h-full"></img>
-        <p className="py-4 text-xl">Hardhat</p>
+        <p className="py-4">Hardhat</p>
       </SkillCard>
-      <SkillCard className="h-60">
+      <SkillCard className="h-full lg:h-60">
         <img src={"/skills/subgraphs.svg"} className="w-full h-full"></img>
-        <p className="py-4 text-xl">Subgraphs</p>
+        <p className="py-4">Subgraphs</p>
       </SkillCard>
-      <SkillCard className="h-60">
+      <SkillCard className="h-full lg:h-60">
         <img src={"/skills/ethers.svg"} className="w-full h-full"></img>
-        <p className="py-4 text-xl">Ethers.js</p>
+        <p className="py-4">Ethers.js</p>
       </SkillCard>
-      <SkillCard className="h-60">
+      <SkillCard className="h-full lg:h-60">
         <FontAwesomeIcon
           icon={faReact}
           className="w-full h-full"
         ></FontAwesomeIcon>
-        <p className="py-4 text-xl">React.js</p>
+        <p className="py-4">React.js</p>
       </SkillCard>
-      <SkillCard className="h-60">
+      <SkillCard className="h-full lg:h-60">
         <FontAwesomeIcon
           icon={faCss3}
           className="w-full h-full"
         ></FontAwesomeIcon>
-        <p className="py-4 text-xl">CSS</p>
+        <p className="py-4">CSS</p>
       </SkillCard>
-      <SkillCard className="h-60">
+      <SkillCard className="h-full lg:h-60">
         <FontAwesomeIcon
           icon={faHtml5}
           className="w-full h-4/5"
         ></FontAwesomeIcon>
-        <p className="py-4 text-xl">HTML</p>
+        <p className="py-4">HTML</p>
       </SkillCard>
-      <SkillCard className="h-60">
+      <SkillCard className="h-full lg:h-60">
         <FontAwesomeIcon
           icon={faJs}
           className="w-full h-full"
         ></FontAwesomeIcon>
-        <p className="py-4 text-xl">Javascript</p>
+        <p className="py-4">Javascript</p>
       </SkillCard>
-      <SkillCard className="h-60">
+      <SkillCard className="h-full lg:h-60">
         <FontAwesomeIcon
           icon={faPython}
           className="w-full h-full"
         ></FontAwesomeIcon>
-        <p className="py-4 text-xl">Python</p>
+        <p className="py-4">Python</p>
       </SkillCard>
     </div>
   );
