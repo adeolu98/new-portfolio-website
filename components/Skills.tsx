ae27222350044a14108aaafbc20d2dd011026cdb
dCRYPT <39372980+adeolu98@users.noreply.github.com>
2023-01-25 11:18:55 +0100

    remove padding bottom

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -19,66 +19,66 @@ export const Skills: FunctionComponent<SkillsProps> = ({ className = "" }) => {
     <div className={`${className} grid gap-4 grid-cols-3  md:grid-cols-6 text-sm md:text-base lg:text-xl`}>
       <SkillCard className="h-full lg:h-60">
         <img src={"/skills/tailwindcss.svg"} className="w-full h-full"></img>
-        <p className="py-4">Tailwind</p>
+        <p className="pt-4">Tailwind</p>
       </SkillCard>
       <SkillCard className="h-full lg:h-60">
         <img src={"/skills/typescript.svg"} className="w-full h-full"></img>
-        <p className="py-4">Typescript</p>
+        <p className="pt-4">Typescript</p>
       </SkillCard>
       <SkillCard className="h-full lg:h-60">
         <img src={"/skills/nextjs.svg"} className="w-full h-full"></img>
-        <p className="py-4">Next.js</p>
+        <p className="pt-4">Next.js</p>
       </SkillCard>
       <SkillCard className="h-full lg:h-60">
           <img src={"/skills/solidity.svg"} className="w-full h-full"></img>
-          <p className="py-4">Solidity</p>
+          <p className="pt-4">Solidity</p>
       </SkillCard>
       <SkillCard className="h-full lg:h-60">
         <img src={"/skills/hardhat.svg"} className="w-full h-full"></img>
-        <p className="py-4">Hardhat</p>
+        <p className="pt-4">Hardhat</p>
       </SkillCard>
       <SkillCard className="h-full lg:h-60">
         <img src={"/skills/subgraphs.svg"} className="w-full h-full"></img>
-        <p className="py-4">Subgraphs</p>
+        <p className="pt-4">Subgraphs</p>
       </SkillCard>
       <SkillCard className="h-full lg:h-60">
         <img src={"/skills/ethers.svg"} className="w-full h-full"></img>
-        <p className="py-4">Ethers.js</p>
+        <p className="pt-4">Ethers.js</p>
       </SkillCard>
       <SkillCard className="h-full lg:h-60">
         <FontAwesomeIcon
           icon={faReact}
           className="w-full h-full"
         ></FontAwesomeIcon>
-        <p className="py-4">React.js</p>
+        <p className="pt-4">React.js</p>
       </SkillCard>
       <SkillCard className="h-full lg:h-60">
         <FontAwesomeIcon
           icon={faCss3}
           className="w-full h-full"
         ></FontAwesomeIcon>
-        <p className="py-4">CSS</p>
+        <p className="pt-4">CSS</p>
       </SkillCard>
       <SkillCard className="h-full lg:h-60">
         <FontAwesomeIcon
           icon={faHtml5}
           className="w-full h-4/5"
         ></FontAwesomeIcon>
-        <p className="py-4">HTML</p>
+        <p className="pt-4">HTML</p>
       </SkillCard>
       <SkillCard className="h-full lg:h-60">
         <FontAwesomeIcon
           icon={faJs}
           className="w-full h-full"
         ></FontAwesomeIcon>
-        <p className="py-4">Javascript</p>
+        <p className="pt-4">Javascript</p>
       </SkillCard>
       <SkillCard className="h-full lg:h-60">
         <FontAwesomeIcon
           icon={faPython}
           className="w-full h-full"
         ></FontAwesomeIcon>
-        <p className="py-4">Python</p>
+        <p className="pt-4">Python</p>
       </SkillCard>
     </div>
   );
