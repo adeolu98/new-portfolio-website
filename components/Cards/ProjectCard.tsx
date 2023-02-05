bce32ec4d54bc3374945f40cee436101fecf6efc
dCRYPT <39372980+adeolu98@users.noreply.github.com>
2023-02-05 11:30:40 +0100

    add card style for mobile

--- components/Cards/ProjectCard.tsx
+++ components/Cards/ProjectCard.tsx
@@ -16,26 +16,46 @@ export const ProjectCard: FunctionComponent<ProjectCardProps> = ({
   const [showProjectInfo, setShowProjectInfo] = useState("invisible");
 
   const handleDisplayProjectInfo = (action: string) => {
-    !disableHover && action === "show" 
+    !disableHover && action === "show"
       ? setShowProjectInfo("visible")
       : setShowProjectInfo("invisible");
   };
 
   return (
-    <div
-      className={`${className} border rounded-3xl flex flex-col justify-left bg-black`}
-      onMouseOver={() => handleDisplayProjectInfo("show")}
-      onMouseOut={() => handleDisplayProjectInfo("hide")}
-    >
-      <img
-        src={imageSrc}
-        className={`w-full h-full rounded-3xl ${ !disableHover && showProjectInfo === 'visible'  && 'opacity-30'}`}
-        alt={"project"}
-      ></img>
+    <div>
       <div
-        className={`${showProjectInfo} absolute bottom-0 md:w-72 lg:w-96 xl:w-[500px] text-left text-white px-6 pb-6`}
+        className={`${className} border hidden md:block rounded-3xl flex flex-col justify-left bg-black`}
+        onMouseOver={() => handleDisplayProjectInfo("show")}
+        onMouseOut={() => handleDisplayProjectInfo("hide")}
       >
-        {children}
+        <img
+          src={imageSrc}
+          className={`w-full h-full rounded-3xl ${
+            !disableHover && showProjectInfo === "visible" && "opacity-30"
+          }`}
+          alt={"project"}
+        ></img>
+        <div
+          className={`${showProjectInfo} absolute bottom-0 md:w-72 lg:w-96 xl:w-[500px] text-left text-white px-6 pb-6`}
+        >
+          {children}
+        </div>
+      </div>
+      <div className="block md:hidden">
+        <div
+          className={`${className} border rounded-3xl flex flex-col justify-left bg-black`}
+        >
+          <img
+            src={imageSrc}
+            className={`w-full h-full rounded-3xl`}
+            alt={"project"}
+          ></img>
+        </div>
+        <div
+          className={` w-full text-center text-black mt-8`}
+        >
+          {children}
+        </div>
       </div>
     </div>
   );
